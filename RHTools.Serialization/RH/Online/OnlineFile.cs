36b3d482f126b4f0e422c78183bcb8368da84e72
Panzerhandschuh <TylerBeff@gmail.com>
2018-05-01 21:50:29 -0400

    Added online file serialization (not sure if this file is even needed)

---
+++ RHTools.Serialization/RH/Online/OnlineFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHTools.Serialization.RH
+{
+	public class OnlineFile : IBinarySerializable
+	{
+		public byte version;
+		public List<RhGuid> fileGuids;
+
+		public OnlineFile()
+		{
+			fileGuids = new List<RhGuid>();
+		}
+
+		public void Serialize(BinaryWriter writer)
+		{
+			writer.Write(version);
+			foreach (RhGuid fileGuid in fileGuids)
+				writer.Write(fileGuid);
+		}
+
+		public static OnlineFile Deserialize(BinaryReader reader)
+		{
+			OnlineFile file = new OnlineFile();
+
+			file.version = reader.ReadByte();
+			while (reader.BaseStream.Position != reader.BaseStream.Length)
+				file.fileGuids.Add(reader.ReadRhGuid());
+
+			return file;
+		}
+	}
+}
