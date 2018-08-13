23bfc543b6a33bfc4994c76c7d006265a285cb8d
Panzerhandschuh <TylerBeff@gmail.com>
2018-08-13 13:51:28 -0400

    Added RuleGroup, for grouping rules and conditions

---
+++ RHTools.Randomizer/RuleGroup.cs
@@ -0,0 +1,28 @@
+﻿using RHTools.Randomizer.Conditions;
+using RHTools.Randomizer.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RHTools.Randomizer
+{
+	public class RuleGroup
+	{
+		public List<Rule> rules;
+		public List<Condition> conditions;
+
+		public RuleGroup()
+		{
+			rules = new List<Rule>();
+			conditions = new List<Condition>();
+		}
+
+		public RuleGroup(List<Rule> rules, List<Condition> conditions)
+		{
+			this.rules = rules;
+			this.conditions = conditions;
+		}
+	}
+}
